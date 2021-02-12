c49c490af04eb67dc0bc38817110c58bb52b37d7
Kateřina Churanová <katerina@churanova.eu>
2021-02-12 19:30:40 +0100

    fix: kinda works, did some cleanup and small refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::ops::Try;
-
-use actix_web::{error, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer};
 mod models;
 mod settings;
+use actix_web::error::JsonPayloadError;
+use fcm::FcmResponse;
 use models::{ErrCode, MatrixError, PushGatewayResponse, PushNotification};
 use settings::Settings;
 
@@ -26,22 +26,24 @@ async fn process_notification(
             }
         })
         .collect();
-    if (registration_ids.is_empty()) {
+
+    if registration_ids.is_empty() {
         return Err(MatrixError {
             error: String::from("No registration IDs with the matching app ID provided"),
-            errcode: ErrCode::M_BAD_JSON,
+            errcode: ErrCode::MBadJson,
         });
     }
 
-    // Weither the push gateway should send only a data message - we have a specific app_id suffix for this
     let first_device = if let Some(device) = push_notification.notification.devices.first() {
         device
     } else {
         return Err(MatrixError {
             error: String::from("No devices were provided"),
-            errcode: ErrCode::M_BAD_JSON,
+            errcode: ErrCode::MBadJson,
         });
     };
+
+    // Weither the push gateway should send only a data message - we have a specific app_id suffix for this
     let data_message_mode = first_device.app_id == format!("{}.data_message", config.app_id);
 
     // Some notifications may just inform the device that there are no more unread rooms
@@ -62,52 +64,50 @@ async fn process_notification(
     } else {
         fcm::MessageBuilder::new_multi(&config.fcm_admin_key, &registration_ids)
     };
-
     // Set the data for fcm here
-    builder.collapse_key(config.collapse_key);
-    builder.data(&push_notification.notification);
-    builder.badge(unread_string);
-    builder.click_action(config.fcm_notification_click_action);
-    builder.data(&map);
+    builder.collapse_key(&config.fcm_collapse_key);
+    builder
+        .data(&push_notification.notification)
+        .map_err(|e| MatrixError {
+            errcode: ErrCode::MBadJson,
+            error: e.to_string(),
+        })?;
+    // builder.data(&map);
 
     // Set additional keys for the notification message
+
     if !data_message_mode && !is_clearing_notification {
-        let title = str::replace(config.fcm_notification_title.copy(), "count", unread_string);
-        builder.title(title);
-        builder.body(config.fcm_notification_body);
-        builder.badge(unread_string);
-        builder.sound(config.fcm_notification_sound);
-        builder.icon(config.fcm_notification_icon);
-        builder.tag(config.fcm_notification_tag);
-        builder.android_channel_id(config.fcm_notification_android_channel_id);
+        let mut notification = fcm::NotificationBuilder::new();
+        notification
+            .title(&config.fcm_notification_title)
+            .click_action(&config.fcm_notification_click_action)
+            .body(&config.fcm_notification_body)
+            .badge(&unread_string)
+            .sound(&config.fcm_notification_sound)
+            .icon(&config.fcm_notification_icon)
+            .tag(&config.fcm_notification_tag);
+
+        builder.notification(notification.finalize());
     }
 
     // Send the fcm message
-    let response = fcm_client.send(builder.finalize()).await;
 
-    if response.ok {
-        let mut rejected: Vec<String> = Vec::new();
-        if response
-            .into_result()
+    if let Ok(FcmResponse {
+        results: Some(results),
+        ..
+    }) = fcm_client.send(builder.finalize()).await
+    {
+        let rejected: Vec<String> = results
             .iter()
-            .any(|response| response.error.ok)
-        {
-            rejected.append(
-                push_notification
-                    .notification
-                    .devices
-                    .first()
-                    .unwrap()
-                    .pushkey
-                    .clone(),
-            );
-        }
-        return Ok(HttpResponse::Ok().json(PushGatewayResponse { rejected: rejected }));
+            .filter_map(|result| result.error.and_then(|_| result.registration_id.clone()))
+            .collect();
+
+        Ok(HttpResponse::Ok().json(PushGatewayResponse { rejected }))
     } else {
-        return Err(MatrixError {
+        Err(MatrixError {
             error: String::from("Invalid response from upstream push service"),
-            errcode: ErrCode::M_UNKNOWN,
-        });
+            errcode: ErrCode::MUnknown,
+        })
     }
 }
 
@@ -122,13 +122,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(app_config.clone())
             .app_data(fcm_client.clone())
-            .app_data(web::JsonConfig::default().error_handler(|err, req| {
-                MatrixError {
-                    error: err.to_string(),
-                    errcode: ErrCode::M_BAD_JSON,
-                }
-                .into()
-            }))
+            .app_data(web::JsonConfig::default().error_handler(json_error_handler))
             .route(
                 "/_matrix/push/v1/notify",
                 web::post().to(process_notification),
@@ -137,4 +131,21 @@ async fn main() -> std::io::Result<()> {
     .bind(("127.0.0.1", config.server_port))?
     .run()
     .await
+}
+
+fn json_error_handler(err: JsonPayloadError, _: &actix_web::HttpRequest) -> actix_web::Error {
+    if let JsonPayloadError::Deserialize(deserialize_err) = &err {
+        if deserialize_err.classify() == serde_json::error::Category::Data {
+            return MatrixError {
+                error: deserialize_err.to_string(),
+                errcode: ErrCode::MMissingParam,
+            }
+            .into();
+        }
+    }
+    MatrixError {
+        error: err.to_string(),
+        errcode: ErrCode::MBadJson,
+    }
+    .into()
 }
